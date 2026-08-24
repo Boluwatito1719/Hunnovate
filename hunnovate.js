a98262c4438551b6f29629195eca79d27e46a8ac
Boluwatito1719 <boluwatitoakinnuoye31@gmail.com>
2026-08-24 12:33:29 +0100

    Add files via upload

--- hunnovate.js
+++ hunnovate.js
@@ -1,18 +1,18 @@
- const TheGen=document.getElementById("TheGen");
- const mysub=document.getElementById("mysub")
+const TheGen = document.getElementById("TheGen");
+const mysub = document.getElementById("mysub");
 
- mysub.onclick=function HunnovatePassword(){
-    const word="abcdefghijklmnopqrstuvwxyz";
-const alpha="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const num="0123456789";
-const special="!@#$%^&*()_+~`|}{[]:;?><,./-=";
-
-const Random=Math.floor(Math.random()*word.length);
-const Random1=Math.floor(Math.random()*alpha.length);
-const Random2=Math.floor(Math.random()*num.length);
-const Random3=Math.floor(Math.random()*special.length);
-
- let clicks="Hunnovate"+word[Random]+alpha[Random1]+num[Random2]+special[Random3]+num[Random2]+word[Random];
- TheGen.textContent=clicks;
- return clicks
- }
+mysub.onclick = function HunnovatePassword() {
+    const word = "Example"; // Define your word variable here
+    const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    const num = "0123456789";
+    const special = "=$@&#";
+    
+    const Random = Math.floor(Math.random() * word.length);
+    const Random1 = Math.floor(Math.random() * alpha.length);
+    const Random2 = Math.floor(Math.random() * num.length);
+    const Random3 = Math.floor(Math.random() * special.length);
+    
+    let clicks = "Hunno" + "_" + word[Random] + alpha[Random1] + num[Random2]+"_"+ special[Random3] + special[Random3];
+    TheGen.textContent = clicks;
+    return clicks;
+};
